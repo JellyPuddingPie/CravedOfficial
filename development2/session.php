a7e7fedc87292d2d149fbda9499829a53295472b
jonasstams <jonasstams@gmail.com>
2016-02-28 14:02:10 +0100

    index.php gui aangepast

--- development2/session.php
+++ development2/session.php
@@ -1,6 +1,5 @@
 
 <?php
-session_start();
 
 
 
@@ -15,7 +14,7 @@ $session_user =$_SESSION['username'];
 $session_user_id = $_SESSION['user_id'];
 $file = 'login.log';
 // The new person to add to the file
-$person = $_session["username"];
+$person = $_SESSION["username"];
 // Write the contents to the file, 
 // using the FILE_APPEND flag to append the content to the end of the file
 // and the LOCK_EX flag to prevent anyone else writing to the file at the same time
